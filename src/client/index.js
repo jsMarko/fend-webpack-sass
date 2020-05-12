9ab7fb58d3268c18130f1529a2b029a5ed67a55c
Mark Branscum <mbstudios@hotmail.com>
2020-05-12 16:04:23 -0500

    removed the alerts and console.logs that were not needed

--- src/client/index.js
+++ src/client/index.js
@@ -5,8 +5,3 @@ import "./styles/footer.scss"
 import "./styles/form.scss"
 import "./styles/header.scss"
 import "./styles/resets.scss"
-
-console.log(checkForName)
-
-alert("I EXIST")
-console.log("CHANGE!!")
